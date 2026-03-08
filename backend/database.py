722cd80a53495694f3158c5bbc37df0c498aea43
subashininagalingam <subanagalingam.dev@gmail.com>
2026-03-08 13:03:17 +0530

    updated to database

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine, Column, Integer, String, DateTime
 from sqlalchemy.orm import declarative_base, sessionmaker
 from datetime import datetime
-import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+
+DATABASE_URL = "postgresql://postgres:suba@localhost:5432/visual"
 #DATABASE_URL = "postgresql://postgres:Bharathi@localhost:5432/visual"
 
 engine = create_engine(DATABASE_URL)
