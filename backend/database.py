925202146d930019d61da4863aa849a4fd945034
subashininagalingam <subanagalingam.dev@gmail.com>
2026-03-08 13:10:27 +0530

    database url fix

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,11 @@
-from sqlalchemy import create_engine, Column, Integer, String, DateTime
-from sqlalchemy.orm import declarative_base, sessionmaker
-from datetime import datetime
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-
-DATABASE_URL = "postgresql://postgres:suba@localhost:5432/visual"
-#DATABASE_URL = "postgresql://postgres:Bharathi@localhost:5432/visual"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
